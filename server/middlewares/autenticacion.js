4d04c5485c461acdf1d4a56749247795db4f9328
Your Name <you@example.com>
2019-12-26 16:03:54 -0600

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -45,8 +45,34 @@ let verificaAdmin_Role = (req, res, next) => {
 
 }
 
+//================
+// VERIFICAR TOKEN PARA IMAGEN
+//================
+
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    token: token,
+                    message: 'Token no valido'
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+
+}
+
 
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
